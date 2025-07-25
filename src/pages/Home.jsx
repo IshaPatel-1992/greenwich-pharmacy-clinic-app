7d26181dd0628bbbd62a543b083fa11f9948268f
IshaPatel-1992 <patelisha1992@gmail.com>
2025-07-25 12:28:16 -0600

    added new home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import HeroSection from "../components/HeroSection";
+import React from 'react';
+import HeroSection from '../components/HeroSection';
 
 export default function Home() {
   return (
-    <main className="pt-20">
-      {/* pt-20 adds padding so content is below fixed navbar */}
+    <div>
       <HeroSection />
-    </main>
+    </div>
   );
 }
